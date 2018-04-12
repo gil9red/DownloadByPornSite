e272212d0a355b522829afc004d8dd680c35afbd
Ilya Petrash <ilya.petrash@inbox.ru>
2018-04-12 13:18:23 +0500

    Update DownloadByPornSite.py

--- DownloadByPornSite.py
+++ DownloadByPornSite.py
@@ -12,9 +12,11 @@ import argparse
 def sizeof_fmt(num):
     for x in ['bytes', 'KB', 'MB', 'GB']:
         if num < 1024.0:
-            return "%3.1f%s" % (num, x)
+            return "%3.1f %s" % (num, x)
+
         num /= 1024.0
-    return "%3.1f%s" % (num, 'TB')
+
+    return "%3.1f %s" % (num, 'TB')
 
 
 def progress(count, block_size, total_size):
